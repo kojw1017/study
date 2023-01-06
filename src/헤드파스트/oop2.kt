f9c27a2ae00cd2d5d5e3cb96a8d5f7f9e470e8dd
kojw <kojw@mobility42.io>
2023-01-06 10:55:47 +0900

    ..

--- src/헤드파스트/oop2.kt
+++ src/헤드파스트/oop2.kt
@@ -2,10 +2,19 @@ package 헤드파스트
 
 import kotlin.concurrent.timer
 
+/*
+ 194p
+ 유스케이스
+ */
 class DogDoor(private var open: Boolean = false){
     fun open(){
         println("The dog door open")
         open = true
+        //설계에따라 문이 스스로 닫히게 한다
+        timer(period = 1000, initialDelay = 4000){
+            close()
+            cancel()
+        }
     }
     fun close(){
         println("The dog door close")
@@ -14,22 +23,35 @@ class DogDoor(private var open: Boolean = false){
     fun isOpen() = open
 }
 
+//버튼 눌렀을때
 class Remote(private var door: DogDoor){
     fun pressButton(){
         println("Pressing the remote control button..")
         if(door.isOpen()) door.close() else door.open()
-        timer(period = 1000, initialDelay = 5000){
-            door.close()
-            cancel()
-        }
     }
+}
+
+//개가 짖었을때
+class BarkRecognizer(private val door: DogDoor){
+    fun recognize(bark: String){
+        println("BarkRecognizer: Heard a $bark")
+        door.open()
+    }
+}
+
+//개짖는소리
+class Bark(private val sound: String){
+    fun getSound() = sound
+    fun equalsBark(bark: Any) = if(bark is Bark) bark.sound  == this.sound else false
 }
 
 fun main() {
     val door = DogDoor()
+    val recognizer = BarkRecognizer(door)
     val remote = Remote(door)
-    println("1")
-    remote.pressButton()
+    println("Fido starts barking")
+    recognizer.recognize("Woof")
+//    remote.pressButton()
     println("2")
     println("3")
     println("4")
