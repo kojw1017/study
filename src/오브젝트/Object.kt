c033da06b1891b8306bde3b30bfc6556c6d1002e
kojw <kojw@mobility42.io>
2023-03-04 14:05:05 +0900

    ..

---
+++ src/오브젝트/Object.kt
@@ -0,0 +1,34 @@
+package 오브젝트
+
+//45p
+import java.time.LocalTime
+class Audience(var bag: Bag)
+class Bag(private var amount: Long, private val invitation: Invitation? = null){
+    private var ticket: Ticket? = null
+    fun hasInvitation() = invitation != null
+    fun hasTicket() = ticket != null
+    val setTicket = { ticket:Ticket -> this.ticket = ticket }
+    val minusAmount = { amount: Long -> this.amount -= amount }
+    val plusAmount = { amount: Long -> this.amount += amount }
+}
+class Invitation(val invitationDate: LocalTime)
+class Ticket(var fee: Long)
+class TicketSeller(private val ticketOffice: TicketOffice){
+    fun getTicketOffice() = ticketOffice
+}
+class TicketOffice(private var amount: Long, private val tickets: MutableList<Ticket>){
+    fun getTicket() = tickets.removeAt(0)
+    val minusAmount = { amount: Long -> this.amount -= amount }
+    val plusAmount = { amount: Long -> this.amount += amount }
+}
+
+class Theater(val ticketSeller: TicketSeller){
+    fun enter(audience: Audience){
+        if(audience.bag.hasInvitation()){
+            val ticket = ticketSeller.getTicketOffice().getTicket()
+            audience.bag.setTicket(ticket)
+        }else{
+
+        }
+    }
+}
