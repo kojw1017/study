03a89a146b4828ad2a93c0f4128ab12fa8ecd863
kojw <kojw@mobility42.io>
2023-03-27 10:58:03 +0900

    ..

--- src/코어코틀린/141.kt
+++ src/코어코틀린/141.kt
@@ -1,6 +1,17 @@
 package 코어코틀린
-//171P
+
+//193P
+
+class LongIterator(private val from: Long, private val to: Long): Iterator<Long>{
+    private var curr = from
+    override fun hasNext(): Boolean = curr <= to
+    override fun next(): Long = curr++
+}
 
 fun main() {
-
+    val iter = LongIterator(0L, 10L)
+    while (iter.hasNext()){
+        print("${iter.next()},")
+    }
+    println()
 }
