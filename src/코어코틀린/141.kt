e352303c20b966c74f5c3428ee1970ae38602eaf
kojw <kojw@mobility42.io>
2023-04-13 19:59:29 +0900

    ..

--- src/코어코틀린/141.kt
+++ src/코어코틀린/141.kt
@@ -2,15 +2,23 @@ package 코어코틀린
 
 //336p
 
+fun remoteProcess1(): Boolean = kotlin.random.Random.nextBoolean()
+fun remoteProcess2(): Boolean = kotlin.random.Random.nextBoolean()
+fun otherProcess(): String? = if(kotlin.random.Random.nextBoolean()) null
 
+else kotlin.random.Random.nextInt().toString()
+fun process1() = if (remoteProcess1()) throw Throwable("otherProcess()에서 정상 처리가 되지 않음") else "처리 정상 종료"
+fun process2(): String {
+    val midResult: Int = otherProcess()?.split("2")?.size ?: return ""
+    return "결과는 = $midResult"
+}
 fun main() {
-    val words = "This is an example of a sentence".split(" ")
-    println(words)
-    val charList = words.map{ it.toList() }
-    println(charList.flatMap{ it.mapIndexed { i, c ->
-        println(i)
-        println(c)
-    }})
-    val firstLetterCapitalizedCharList = charList.flatMap{it.mapIndexed{index, c
-        -> if(index==0) c.uppercaseChar() else c}}
+    outer@ for (i in 2..9) {
+        println("start outer loop $i")
+        if (i % 2 == 0) continue
+        inner@ for (j in 2..9) {
+            println("start inner loop $j")
+            if (j % 2 == 0) continue@outer
+        }
+    }
 }
