452d0d5a205e91ad81c968acefc1c0083b9b391c
kojw <kojw@mobility42.io>
2023-09-15 11:26:22 +0900

    ..

---
+++ src/코테/공원산책.kt
@@ -0,0 +1,20 @@
+package ㅋ
+class Solution {
+    fun solution(park: Array<String>, routes: Array<String>): IntArray {
+        var answer: IntArray = intArrayOf()
+
+        val parkArray = park.map { it.map { char -> char.toString() }.toTypedArray() }.toTypedArray()
+        parkArray.forEachIndexed { i, strings ->
+            strings.forEachIndexed { j, s ->
+                if(s == "S") println("$i $j")
+            }
+        }
+
+        return answer
+    }
+}
+fun main(){
+    val v1 = arrayOf("SOO", "OOO", "OOO")
+    val v2 = arrayOf("E 2", "S 2", "W 1")
+    Solution().solution(v1, v2)
+}
