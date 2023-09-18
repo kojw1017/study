cd233869dc93acf13e038538f704591a084f2da1
kojw <kojw@mobility42.io>
2023-09-18 17:13:26 +0900

    ..

--- src/코테/에라토스테네스체.kt
+++ src/코테/에라토스테네스체.kt
@@ -2,19 +2,31 @@ package ㅋㅋ
 class Solution {
     fun solution(park: Array<String>, routes: Array<String>): IntArray {
         var answer: IntArray = intArrayOf()
-
+        val currIdx = mutableListOf<Int>()
         val parkArray = park.map { it.map { char -> char.toString() }.toTypedArray() }.toTypedArray()
         parkArray.forEachIndexed { i, strings ->
             strings.forEachIndexed { j, s ->
-                if(s == "S") println("$i $j")
+                if(s == "S"){
+                    currIdx.add(i)
+                    currIdx.add(j)
+                }
             }
+        }
+        routes.forEach {
+            val road = it.split(" ")
+            val op = when(road[0]){
+                "E","W" -> 1
+                "S","N" -> 0
+                else -> 2
+            }
+            currIdx[op] = currIdx[op]+road[1].toInt()
         }
 
         return answer
     }
 }
 fun main(){
-    val v1 = arrayOf("SOO", "OOO", "OOO")
+    val v1 = arrayOf("OSO", "OOO", "OOO")
     val v2 = arrayOf("E 2", "S 2", "W 1")
     Solution().solution(v1, v2)
 }
