09c28724c9928f7cc5823cddda9761a691c7f55d
kojw <kojw@mobility42.io>
2024-08-07 11:19:37 +0900

    fsfd

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,7 @@ package org.example
 
 // 메인 함수에서 MemberManagementSystem 사용 예시
 fun main() {
+    listOf(2, 4, 0, 8).flatMap { if(it != 0) listOf(16/it) else listOf() }.forEach {
+        println(it)
+    }
 }
