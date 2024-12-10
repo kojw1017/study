557837bbb72a762d75317c2906809668c4ec8b2b
kojw <kojw@mobility42.io>
2024-12-10 11:53:13 +0900

    ..

---
+++ src/main/kotlin/dev/코틀린디자인패턴/8장/8장.kt
@@ -0,0 +1,22 @@
+package org.example.dev.코틀린디자인패턴.`8장`
+
+import kotlinx.coroutines.channels.actor
+import kotlinx.coroutines.runBlocking
+
+//val input = listOf(setOf(1, 2, 3), setOf(3, 1, 2), setOf(2, 3, 1))
+//fun sum(numbers:Set<Int>):Double = numbers.sumOf { it.toDouble() }
+//fun summarizer(): (Set<Int>) -> Double{
+//    val resultCache = mutableMapOf(Set<Int>, Double)()
+//    return { numbers: Set<Int> ->
+//        resultCache.computeIf
+//    }
+//}
+//fun main() {
+//    runBlocking {
+//        val a  = actor<String> {
+//            for (c in channel){
+//
+//            }
+//        }
+//    }
+//}
